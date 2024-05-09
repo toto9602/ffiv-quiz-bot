c1482e5b8812eb5f64777332a6ee94b2114b0f4d
toto9602 <toto9602@naver.com>
2024-05-09 22:58:42 +0900

    chore : ADD emojis to Messages

--- src/main/java/personal/ffivquizbot/slash_command/job_question/JobQuestionCommandHandler.java
+++ src/main/java/personal/ffivquizbot/slash_command/job_question/JobQuestionCommandHandler.java
@@ -9,12 +9,14 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.springframework.stereotype.Service;
+import personal.ffivquizbot.emojis.Emojis;
 import personal.ffivquizbot.event_waiter.EventWaiterProvider;
 import personal.ffivquizbot.slash_command.SlashCommandHandler;
 import personal.ffivquizbot.slash_command.SlashCommands;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -38,7 +40,8 @@ public class JobQuestionCommandHandler implements SlashCommandHandler {
 
         String jobQuestionReply = "[ 잡 ] 관련 문제를 " +
                 questionCount +
-                "개 출제할게요! 5초 뒤 시작합니다!";
+                "개 출제할게요! 5초 뒤 시작합니다!" + Emojis.ROCKET.getEmojiString() + " " + Emojis.ROCKET.getEmojiString();
+
 
         event.reply(jobQuestionReply).queue();
         log.info("reply 전송 완료!");
@@ -63,7 +66,7 @@ public class JobQuestionCommandHandler implements SlashCommandHandler {
 
             System.out.println("파일 업로드 객체 생성");
 
-            String questionDescription = Integer.valueOf(jobIndex+1).toString() + "번 문제";
+            String questionDescription = "[ " + Integer.valueOf(jobIndex+1).toString() + "번 문제" + " ]";
 
             targetChannel.sendMessage(questionDescription)
                     .setEmbeds(embed.build())
@@ -82,7 +85,7 @@ public class JobQuestionCommandHandler implements SlashCommandHandler {
                                         if (authorMessage.equals(jobToAsk.getJobName())) { // 정답
                                             log.info("정답! 정답 count를 높이고, 메시지를 전송합니다.");
 
-                                            targetChannel.sendMessage("정답입니다!! ./박수").queue();
+                                            targetChannel.sendMessage("정답입니다!!! " + Emojis.CLAP.getEmojiString() + " "  + Emojis.CLAP.getEmojiString()).queue();
 
                                             if (!isLast(jobIndex, total)) {
                                                 log.info("문제가 남았습니다! 다음 문제를 출제합니다.");
@@ -92,10 +95,10 @@ public class JobQuestionCommandHandler implements SlashCommandHandler {
                                                 processJobQuestion(targetChannel, targetUser, jobs, updatedJobIndex, total, updatedCorrectCount);
                                             } else {
                                                 // 출제 끝
-                                                targetChannel.sendMessage("수고하셨습니다!! 정답률 : " + getCorrectRate(correctCount, total)).queue();
+                                                targetChannel.sendMessage("수고하셨습니다!! " + Emojis.CLAP.getEmojiString() + " " + Emojis.CLAP.getEmojiString() + "\n" + "정답률 : " + getCorrectRate(correctCount, total)).queue();
                                             }
                                         } else { // 오답
-                                            targetChannel.sendMessage("땡~! 정답은 " + jobToAsk.getJobName() + "입니다!").queue();
+                                            targetChannel.sendMessage("땡~! " + Emojis.WOMAN_X.getEmojiString() + " " + Emojis.WOMAN_X.getEmojiString() + " " + "정답은 [ " + jobToAsk.getJobName() + " ] 입니다!").queue();
 
                                             if (!isLast(jobIndex, total)) {
                                                 log.info("문제가 남았습니다! 다음 문제를 출제합니다.");
@@ -112,9 +115,7 @@ public class JobQuestionCommandHandler implements SlashCommandHandler {
                                     () -> { // Timeout Action
                                         log.info("TIMEOUT!" + jobToAsk.getJobName());
 
-                                        targetChannel.sendMessage("시간 초과입니다! 정답은 " + jobToAsk.getJobName() +"! :)").queue();
-
-
+                                        targetChannel.sendMessage(Emojis.CLOCK.getEmojiString() + " " + "시간 초과입니다!" + Emojis.CLOCK.getEmojiString() + " " + "정답은 [ " + jobToAsk.getJobName() +" ] ! :)").queue();
                                         if (isLast(jobIndex, total)) {
                                             System.out.println("출제를 종료합니다...");
                                             return;
@@ -128,7 +129,7 @@ public class JobQuestionCommandHandler implements SlashCommandHandler {
             sleep(1000);
             System.out.println("다음 문제로 진입!!!");
         } catch (Exception e) {
-            targetChannel.sendMessage("메시지 전송 과정에서 오류가 발생했습니다..ㅠ 개발자를 추궁해 주세요...").queue();
+            targetChannel.sendMessage("메시지 전송 과정에서 오류가 발생했습니다.." + Emojis.SMILING_TEAR.getEmojiString() + " " + "개발자를 추궁해 주세요...").queue();
             e.printStackTrace();
         }
     }
@@ -154,7 +155,7 @@ public class JobQuestionCommandHandler implements SlashCommandHandler {
         log.info("correctCount = {}", correctCount);
         log.info("total = {}", total);
 
-        double rate = (double) correctCount / (double) total * 100;
+        double rate = (double) correctCount / (double) total;
 
         return percentFormat.format(rate);
     }
