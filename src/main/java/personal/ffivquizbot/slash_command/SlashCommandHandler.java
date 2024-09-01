a934aca2bd33ab8d21f02db925ef88df17075014
toto9602 <toto9602@naver.com>
2024-09-01 19:50:25 +0900

    [chore] SlashCommands, SlashCommandHandler 이전

--- src/main/java/personal/ffivquizbot/slash_command/SlashCommandHandler.java
+++
@@ -1,9 +0,0 @@
-package personal.ffivquizbot.slash_command;
-
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-
-public interface SlashCommandHandler {
-    public abstract SlashCommands getCommand();
-
-    public abstract void handleCommand(SlashCommandInteractionEvent event);
-}
---
+++ src/main/kotlin/personal/ffivquizbot/slashcommand/SlashCommands.kt
@@ -0,0 +1,15 @@
+package personal.ffivquizbot.slashcommand
+
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+
+enum class SlashCommands(
+    val commandName:String,
+    val description:String,
+    val hasOptions:Boolean,
+    val options:OptionData?,
+){
+    JOB_QUESTION("잡", "파판14의 잡 문양 문제를 출제합니다.", true, OptionData(OptionType.INTEGER, "question_count", "문제 개수를 설정합니다. (기본 : 19 종류 전체)").setRequiredRange(1, 19)),
+    INTRODUCTION("설명", "사용 가능한 명령어를 안내합니다.", false, null),
+    ASTROLOGIAN("점지", "점지 스킬의 카드 관련 문제를 출제합니다.", true,  OptionData(OptionType.INTEGER, "question_count", "문제 개수를 설정합니다. (기본 : 6 종류 전체").setRequiredRange(1,6));
+}
