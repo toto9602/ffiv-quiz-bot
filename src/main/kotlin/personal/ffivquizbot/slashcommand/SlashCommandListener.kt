9df4474f38abf21db086aa777620d35d2ca1ff3f
toto9602 <toto9602@naver.com>
2024-09-06 19:26:47 +0900

    [feat] SlashCommandListener 이전

--- src/main/kotlin/personal/ffivquizbot/slashcommand/SlashCommandListener.kt
+++ src/main/kotlin/personal/ffivquizbot/slashcommand/SlashCommandListener.kt
@@ -4,9 +4,9 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import okhttp3.internal.immutableListOf
 import org.springframework.stereotype.Service
-import personal.ffivquizbot.slash_command.astrologian_skills.AstrologianSkillQuestionCommandHandler
-import personal.ffivquizbot.slash_command.introduction.IntroductionCommandHandler
-import personal.ffivquizbot.slash_command.job_question.JobQuestionCommandHandler
+import personal.ffivquizbot.slashcommand.astrologianskills.AstrologianSkillQuestionCommandHandler
+import personal.ffivquizbot.slashcommand.introduction.IntroductionCommandHandler
+import personal.ffivquizbot.slashcommand.jobquestion.JobQuestionCommandHandler
 
 @Service
 class SlashCommandListener(
@@ -18,8 +18,16 @@ class SlashCommandListener(
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
         val name = event.name
 
-       immutableListOf<SlashCommandHandler>(jobQuestionCommandHandler, introductionCommandHandler, astrologianSkillQuestionCommandHandler)
+        val handler = immutableListOf<SlashCommandHandler>(
+            jobQuestionCommandHandler,
+            introductionCommandHandler,
+            astrologianSkillQuestionCommandHandler
+        ).firstOrNull { handler -> handler.command.toString() == name }
 
+        if (handler == null) {
+            throw IllegalArgumentException("지원하지 않는 명령어가 입력되었습니다, ${name}")
+        }
+
+        handler.handleCommand(event)
     }
-
 }
